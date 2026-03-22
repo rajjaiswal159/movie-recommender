93f90cd2ea91c32a6c37f78446c6f22bfacd28fa
rajjaiswal159 <rajjeswal31@gmail.com>
2026-03-22 09:54:49 +0530

    Refactor movie recommendation API code

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ def recommend_movie(movie: str, n: int):
 
     if idx is not None:
         sim_scores = SIM_MATRIX[idx]
-        indices = np.argsort(sim_scores)[::-1][0:n]
+        indices = np.argsort(sim_scores)[::-1][:n]
 
         if len(indices) == 0:
             raise HTTPException(status_code=404, detail="No recommendations found")
